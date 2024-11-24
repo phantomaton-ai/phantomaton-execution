df1ea1db23fe61ae1bc488d07d0f2be63c8a17d9
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-23 19:46:13 -0800

    Updated by Phantomaton

---
+++ defaults.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import defaults from './defaults.js';
+
+describe('Execution Defaults', () => {
+  it('provides default configuration values', () => {
+    expect(defaults.header).to.equal('Command execution');
+    expect(defaults.message).to.equal('Executable commands are available in this environment.');
+    expect(defaults.symbols.directive.start).to.equal('🪄✨ ');
+    expect(defaults.symbols.directive.end).to.equal('⚡️');
+  });
+});
